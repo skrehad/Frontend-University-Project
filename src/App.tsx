c5f01567751190d194f86adbc3d84e68c806b1c6
rehadhasan <rehadhasan664@gmail.com>
2025-01-08 00:39:20 +0600

    added nabvar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import MainLayout from "./components/layout/MainLayout";
+
 function App() {
-  return (
-    <>
-      <h1>PH University</h1>
-    </>
-  );
+  return <MainLayout></MainLayout>;
 }
 
 export default App;
