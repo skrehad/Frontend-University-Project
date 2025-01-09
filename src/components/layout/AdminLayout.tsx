f23311f44697a29578f84aa1bf6a387288a155af
rehadhasan <rehadhasan664@gmail.com>
2025-01-09 18:00:04 +0600

    change admin layout

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const AdminLayout = () => {
   return (
     <div>
-      <h1>This is navbar</h1>
+      <h1>This is admin layout</h1>
       <Outlet></Outlet>
     </div>
   );
