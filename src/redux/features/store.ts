f9086bcc09ae336bba3472d14e83e9a907705386
rehadhasan <rehadhasan664@gmail.com>
2025-01-12 04:44:09 +0600

    connect baseApi

--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
+import { baseApi } from "../api/baseApi";
 
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
     auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
