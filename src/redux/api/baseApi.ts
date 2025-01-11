2003978f130a7a3e485b8811f8a995a161e5bbe4
rehadhasan <rehadhasan664@gmail.com>
2025-01-12 04:07:53 +0600

    create authApi another folder

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,15 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseApi = createApi({
+export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
-  endpoints: (builder) => ({
-    login: builder.mutation({
-      query: (userInfo) => ({
-        url: "/auth/login",
-        method: "POST",
-        body: userInfo,
-      }),
-    }),
-  }),
+  endpoints: () => ({}),
 });
