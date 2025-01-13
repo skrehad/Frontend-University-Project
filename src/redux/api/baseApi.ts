713e28c0fa1647193642fe5287faec3298891ab3
rehadhasan <rehadhasan664@gmail.com>
2025-01-14 04:24:53 +0600

    refreshing token in base api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -33,8 +33,9 @@ const baseQueryWithRefreshToken: BaseQueryFn<
 
   if (result?.error?.status === 401) {
     //* Send Refresh
-    console.log("Sending refresh token");
+    // console.log("Sending refresh token");
 
+    // set token after token has expired by refreshing token
     const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
       method: "POST",
       credentials: "include",
