a53fd24ea24dfc59f53ffb231c9e59d6da16eab7
rehadhasan <rehadhasan664@gmail.com>
2025-01-19 00:53:16 +0600

    successfully get academic faculty

--- src/pages/admin/academicManagement/AcademicFaculty.tsx
+++ src/pages/admin/academicManagement/AcademicFaculty.tsx
@@ -1,7 +1,68 @@
+import { useState } from "react";
+import { TAcademicFaculty, TQueryParam } from "../../../types";
+import { useGetAcademicFacultiesQuery } from "../../../redux/features/admin/academicManagement.api";
+import { Button, Input, Table, TableColumnsType } from "antd";
+
+export type TTableData = Pick<TAcademicFaculty, "name">;
+
 const AcademicFaculty = () => {
+  const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+  const {
+    data: academicFaculty,
+    isLoading,
+    isFetching,
+  } = useGetAcademicFacultiesQuery(params);
+
+  console.log({ academicFaculty, isLoading, isFetching });
+
+  const tableData = academicFaculty?.data?.map(({ _id, name }) => ({
+    key: _id,
+    name,
+  }));
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Name",
+      key: "name",
+      dataIndex: "name",
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: (record) => (
+        <div>
+          <Button onClick={() => handleUpdate(record.key)}>Update</Button>
+        </div>
+      ),
+    },
+  ];
+
+  const handleSearch = (searchValue: string) => {
+    const updatedParams: TQueryParam[] = searchValue
+      ? [{ name: "name", value: searchValue }]
+      : [];
+    setParams(updatedParams);
+  };
+
+  const handleUpdate = (id: string) => {
+    console.log(`Update clicked for ID: ${id}`);
+  };
+
   return (
     <div>
-      <h1> This is AcademicFaculty component </h1>
+      <Input.Search
+        placeholder="Search by name"
+        onSearch={handleSearch}
+        allowClear
+        style={{ marginBottom: 16 }}
+      />
+
+      <Table
+        loading={isFetching}
+        columns={columns}
+        dataSource={tableData}
+        rowKey="key"
+      />
     </div>
   );
 };
