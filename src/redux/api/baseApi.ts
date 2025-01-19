89248c7a3f3dd7baadba8b5f2f9ceab9d9cbca82
rehadhasan <rehadhasan664@gmail.com>
2025-01-20 05:05:26 +0600

    update base api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -72,5 +72,6 @@ const baseQueryWithRefreshToken: BaseQueryFn<
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: baseQueryWithRefreshToken,
+  tagTypes: ["semester", "courses"],
   endpoints: () => ({}),
 });
